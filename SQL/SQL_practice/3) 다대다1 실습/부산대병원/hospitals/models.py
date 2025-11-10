d110640f64d09557579fed57b72353d05567442c
brightonlog <fls4804@gmail.com>
2025-11-10 12:01:13 +0900

    'through' argumnet added on models.py

--- SQL/SQL_practice/3) 다대다1 실습/부산대병원/hospitals/models.py
+++ SQL/SQL_practice/3) 다대다1 실습/부산대병원/hospitals/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+  name = models.TextField()
+  def __str__(self):
+    return f'{self.pk}번 의사 {self.name}'
+  
+
+
+# 의사와 환자간의 M:N 관계를 Reservation 모델을 통해 관리할게 ~!
+# => throug='Reservation'
+class Patient(models.Model):
+  doctors = models.ManyToManyField(Doctor, through='Reservation')
+  name = models.TextField()
