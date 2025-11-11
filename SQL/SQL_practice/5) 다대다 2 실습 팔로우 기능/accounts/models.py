207df5ab961b23c8946c32db574cc6d5f911483b
brightonlog <fls4804@gmail.com>
2025-11-11 12:20:23 +0900

    5. accounts/models.py

--- SQL/SQL_practice/5) 다대다 2 실습 팔로우 기능/accounts/models.py
+++ SQL/SQL_practice/5) 다대다 2 실습 팔로우 기능/accounts/models.py
@@ -5,5 +5,11 @@ from django.db import models
 # AbstractUser : 자동으로 필드를 제공(로그인, 권한 관리 등)
 from django.contrib.auth.models import AbstractUser
 
+
+# ManyToManyField의 첫번째 인자로 'self'가 들어가면 자기 자신에서 역참조, 참조 관계가 다 있을 수 있음
+# symmetrical=False
+# => 내가 상대를 팔로잉했다고 상대가 나를 팔로우 한 건 아니니까
 class User(AbstractUser):
-    pass
+    followings = models.ManyToManyField(
+        'self', symmetrical=False, related_name = 'followers'
+    )
