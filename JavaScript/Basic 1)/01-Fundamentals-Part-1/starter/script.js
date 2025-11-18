e8a083a603dfbeaac6b6cd71d08601ad83324360
brightonlog <fls4804@gmail.com>
2025-11-18 10:31:07 +0900

    데이터 타입과 변수 선언

--- JavaScript/Basic 1)/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript/Basic 1)/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 40 + 9 + 21; // 자바스크립트에게 얘를 어디에 두라고 한 것이 아니기 때문에 화면에 뜨지 않는다.
 console.log(30 + 3)
@@ -11,3 +12,27 @@ let firstName = 'Jonas';
 // let new = 2; // 이것도 오류 뜸. 왜냐? 예약어라서.
 
 let PI = 3.1415; // 파이같은 경우는 바뀌지 않는 상수이므로, 대문자로 쓰는 게 관례임
+
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun); // 불리언
+
+console.log(typeof true);                 // 불리언
+console.log(typeof javascriptIsFun);      // 불리언
+console.log(typeof 23);                   // 숫자
+console.log(typeof 'Jonas');              // 문자열
+
+
+let year;
+console.log(year);                        // undefined
+console.log(typeof year);                 // undefined
+
+console.log(typeof null)                  // object 개체? 웬 개체? 오류임.
+
+
+// 변수 선언
+// const job;  이건 오류 뜸. const는 선언하면 부조건 값도 지정해야함
+
+var job = 'programmer';
+job = 'teacher'
