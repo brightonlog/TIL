e37c3ae4ef9b2a15fce0db01926dbab820feab6c
brightonlog <fls4804@gmail.com>
2025-11-18 09:53:56 +0900

    values & variables

---
+++ JavaScript/Basic 1)/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+let js = 'amazing';
+40 + 9 + 21; // 자바스크립트에게 얘를 어디에 두라고 한 것이 아니기 때문에 화면에 뜨지 않는다.
+console.log(30 + 3)
+
+console.log("Jonas");
+console.log("23");
+
+let firstName = 'Jonas';
+
+// let jonas_matilda = 'JM'; // 오류 뜸. 왜냐? 달러, 밑줄이 글자와 숫자 외에 허락된 유일한 기호이기 때문
+// let new = 2; // 이것도 오류 뜸. 왜냐? 예약어라서.
+
+let PI = 3.1415; // 파이같은 경우는 바뀌지 않는 상수이므로, 대문자로 쓰는 게 관례임
