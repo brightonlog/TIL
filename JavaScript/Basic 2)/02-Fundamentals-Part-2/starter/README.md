12415e5e2787048a8e7cf6f69f2e44b323ef632e
brightonlog <fls4804@gmail.com>
2025-11-19 10:45:33 +0900

    함수

---
+++ JavaScript/Basic 2)/02-Fundamentals-Part-2/starter/README.md
@@ -0,0 +1,3 @@
+# 함수
+![alt text](image.png)
+- 함수 작성법 세가지
---
+++ JavaScript/Basic 2)/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,40 @@
+// 함수 안에서 다른 함수 호출하기
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+
+function fruitProcessor(appels,oranges){
+  const applepieces = cutFruitPieces(apples);
+  const orangepieces = cutFruitPieces(oranges);
+
+
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+fruitProcessor(2, 3)
+
+
+// 자바 스크립트 함수 선언할 때 꼭 함수 이름 지정해주지 않아도 됨
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+}
+
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear)
+  const retirement = 65 - age
+
+  if (retirement > 0 ) {
+    return retirement
+    // return retirement;
+  } else {
+    return -1
+  }
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'))
+console.log(yearsUntilRetirement(195 0, 'Mike'))
