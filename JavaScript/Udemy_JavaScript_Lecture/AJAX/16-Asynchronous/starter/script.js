5f458d89113296f7aa3fdec1c27bad7f88292cbb
brightonlog <fls4804@gmail.com>
2025-11-25 09:22:02 +0900

    feat: Implement country data fetching using XMLHttpRequest

--- JavaScript/Udemy_JavaScript_Lecture/AJAX/16-Asynchronous/starter/script.js
+++ JavaScript/Udemy_JavaScript_Lecture/AJAX/16-Asynchronous/starter/script.js
@@ -10,3 +10,37 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+
+const getCountryData = function (country){
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+
+  request.addEventListener('load', function(){
+    console.log(this.responseText);
+
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+            <article class="country">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>${ (+data.population / 1000000).toFixed(1)}</span></p>
+              <p class="country__row"><span>${data.languages[0].name}</span>LANG</p>
+              <p class="country__row"><span>${data.currencies[0].name}</span>CUR</p>
+            </div>
+          </article>
+          `;
+          countriesContainer.insertAdjacentHTML('beforeend', html)
+          countriesContainer.style.opacity = 1;
+
+  })
+}
+
+getCountryData('Britain')
+getCountryData('Germany')
