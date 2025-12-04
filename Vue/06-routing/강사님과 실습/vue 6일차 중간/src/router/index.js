2b62da6f1e4f893252b8825f87ad56066795f4a9
Erin <fls4804@gmail.com>
2025-12-04 14:27:56 +0900

    add : 강사님과 실습 예제 중간 파일 추가

---
+++ Vue/06-routing/강사님과 실습/vue 6일차 중간/src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// 1. 상대경로 2. 절대경로
+import HomeView from '@/views/HomeView.vue'
+import UserView from '@/views/UserView.vue'
+
+import UserHome from '@/components/UserHome.vue'
+import UserPosts from '@/components/UserPosts.vue'
+import UserProfile from '@/components/UserProfile.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/user/:id', // url
+      component: UserView, // views 디렉토리
+      // beforeEnter : 라우트 가드
+      beforeEnter: (to, from) => {
+        console.log(from)
+        console.log(to)
+      },
+      // components 디렉토리
+      children: [
+        {path: '', name: 'user', component: UserHome}, // http://localhost:5173/user/:id
+        {path: 'profile', name: 'user-profile', component: UserProfile}, // http://localhost:5173/user/:id/profile
+        {path: 'posts', name: 'user-posts', component: UserPosts}, // http://localhost:5173/user/:id/posts
+      ]
+    },
+  ],
+})
+
+export default router
---
+++ Vue/06-routing/강사님과 실습/vue 6일차 중간/src/views/UserView.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <RouterLink :to="{ name: 'user-profile'}">Profile</RouterLink>
+    <RouterLink :to="{ name: 'user-posts'}">Posts</RouterLink>
+
+    <div>
+        <h1>UserView</h1>
+        <h2>{{ userId }}번의 User 페이지</h2>
+    </div>    
+
+    <!-- 페이지가 랜더링 -->
+    <RouterView/>
+
+</template>
+
+
+<script setup>
+
+    import { ref } from 'vue'
+
+    import { RouterLink, RouterView } from 'vue-router'
+
+    // useRoute: read-only , useRouter: 라우트 전환을 위한 API
+    import { useRoute, useRouter } from 'vue-router'
+    // 가드 Leave : 페이지 나갈때, Update : 페이지 url 변경 될 때
+    import { onBeforeRouteLeave, onBeforeRouteUpdate } from 'vue-router'
+
+    const route = useRoute()
+    const router = useRouter()
+
+    const userId = ref(route.params.id)
+
+</script>
+
+
+<style scoped>
+
+
+</style>
