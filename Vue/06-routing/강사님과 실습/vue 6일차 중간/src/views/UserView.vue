fb44f937dae9cca9aa72c2263683755ee2ce1215
Yerin Kim <fls4804@gmail.com>
2025-12-04 14:32:20 +0900

    feat: 버튼 기능 구현

--- Vue/06-routing/강사님과 실습/vue 6일차 중간/src/views/UserView.vue
+++ Vue/06-routing/강사님과 실습/vue 6일차 중간/src/views/UserView.vue
@@ -3,10 +3,15 @@
     <RouterLink :to="{ name: 'user-profile'}">Profile</RouterLink>
     <RouterLink :to="{ name: 'user-posts'}">Posts</RouterLink>
 
+    
     <div>
         <h1>UserView</h1>
         <h2>{{ userId }}번의 User 페이지</h2>
     </div>    
+    
+    <button @click='goHome'>Home</button>
+    <button @click='routeUpdata'>99번의 유저 페이지</button>
+
 
     <!-- 페이지가 랜더링 -->
     <RouterView/>
@@ -29,6 +34,12 @@
     const router = useRouter()
 
     const userId = ref(route.params.id)
+    // push와 replace의 차이
+    // push : 히스토리에 추가되기 때문에 이전 페이지로 갈 수 있음 ( 뒤로가기 됨)
+    // replace : 히스토리에 추가되지 않기 때문에 이전 페이지로 돌아갈 수 없음 (뒤로가기 안됨)
+    const goHome = function() {
+        router.replace({name: 'home'})
+    }
 
 </script>
 
