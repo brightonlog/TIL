1f9d2692245601e404cc0d08504f9949a3417ee7
Erin <fls4804@gmail.com>
2025-12-08 13:34:13 +0900

    add: 강사님의 중간 코드 추가

---
+++ Vue/07-pinia/vue 7일차 중간/src/components/TodoListItem.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div>
+    <span>
+      {{ todo.text }}
+    </span>
+  </div>
+
+</template>
+
+<script setup>
+
+  import { useCounterStore } from '@/stores/counter';
+
+  const store = useCounterStore()
+
+  defineProps({
+    todo: Object
+  })
+
+</script>
+
+
+<style scoped>
+
+</style>
